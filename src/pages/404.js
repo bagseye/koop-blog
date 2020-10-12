cf74b30ee6a231952f09094aa90a2aa74e40809c
Morgan Baker <morganbakerdesigner@gmail.com>
2020-10-12 21:20:20 +0100

    Added gatsby browser ssr and browser

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,14 @@
 import React from "react"
-import Layout from "../components/layout"
 import { Link } from "gatsby"
 
 const Error = () => {
   return (
-    <Layout>
+    <>
       <h1>This page has disappeared</h1>
       <Link className="btn" to="/">
         Return Home
       </Link>
-    </Layout>
+    </>
   )
 }
 
