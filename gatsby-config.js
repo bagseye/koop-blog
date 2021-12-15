89fc5035eaeeea0213201a96bcbb43aaf2355e7f
Morgan Baker <morganbakerdesigner@gmail.com>
2021-12-15 00:02:44 +0000

    Added netlify plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
